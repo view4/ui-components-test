87e7ac0902857172c4415f8b5bee091e0dec6348
view4 <morrisgaby987@googlemail.com>
2020-12-18 14:00:06 +0200

    adding dynamic based content

--- src/components/notifications.js
+++ src/components/notifications.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import notificationIndicator from "../assets/notifications.svg";
+import { notifications } from '../constants';
+
+const DropdownElement = ({ notification }) => {
+    const { title, type, target, time } = notification
+    return (
+      <div>
+        <span>Circle here</span>
+        <div> {title} <span> {type} {target} </span></div>
+        <span>{time}</span>
+      </div>
+    );
+  };
+
 
 const Notifications = () => {
+    const [ isActive, setIsActive ] = useState(false);
     return (
         <div>
-            <span>
+            <span onClick={() => !setIsActive(!isActive)}>
                 <img src={notificationIndicator} />
                 <div className={"notification-number"}> 2</div>
             </span>
+            {
+                isActive && notifications.map((notification, i) => <DropdownElement notification={notification} key={i} />)
+            }
         </div>
     )
 }
--- src/components/search.js
+++ src/components/search.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import searchIndicator from "../assets/search.svg"
+import React, { useState } from "react";
+import searchIndicator from "../assets/search.svg";
+import { search } from "../constants";
+import Button from "./button";
+
+const DropdownElement = ({ text }) => {
+  return (
+    <div>
+      <span>col here</span>
+      <span>{text}</span>
+      <span>{">"}</span>
+    </div>
+  );
+};
 
 const Search = () => {
-    return (
-        <div className={"search-container"}>
-            <img src={searchIndicator} />
-                <input type={"text"} />
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <>
+      <div className={"search-container"}>
+        <img src={searchIndicator} />
+        <input type={"text"} onFocus={() => setIsFocused(true)} />
+      </div>
+      {isFocused && (
+        <div>
+          Recent Searches
+          {search.map((searchItem, i) => (
+            <DropdownElement text={searchItem} key={i} />
+          ))}
+          <Button text="Adddress Search" bg={"#6C5DD3"} />
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default Search
+export default Search;
